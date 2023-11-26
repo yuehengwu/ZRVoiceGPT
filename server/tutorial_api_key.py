a7f12d4aa6b60e30f4863b8f4170ac31b1ab23f9
yuehengwu <yuehengwu@tencent.com>
2023-11-26 13:09:31 +0800

    modify some config

--- server/tutorial_api_key.py
+++ server/tutorial_api_key.py
@@ -3,5 +3,5 @@ from openai import OpenAI
 
 # 初始化OpenAI
 client = OpenAI(
-                api_key="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",
+                api_key="your-chatgpt-key",
                 )
